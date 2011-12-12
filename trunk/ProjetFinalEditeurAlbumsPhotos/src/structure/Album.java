5d1f145ddd45443f7e43bb2b57f90d197acb3c8e
sylvain.brunerie@gmail.com <sylvain.brunerie@gmail.com@dc4cb024-c5a2-7f39-0f71-a50d1583f679>
2011-12-12 00:21:44 +0000

    Ajout fichiers Fabrice

--- trunk/ProjetFinalEditeurAlbumsPhotos/src/structure/Album.java
+++ trunk/ProjetFinalEditeurAlbumsPhotos/src/structure/Album.java
@@ -1,6 +1,66 @@
 package structure;
+import java.util.*;
 
-public class Album
-{
+public class Album<A extends IPhoto> {
+	Vector<A> photos;
+	
+	public void rajout(A a){
+		photos.add(a);
+	}
+	
+	public void enleve(String chemin) throws ImageNonExistanteError{
+	int b = -1;
+		for(A a : photos){
+			if(a.getchemin()==chemin){
+				System.out.println(photos.indexOf(a));
+				b=photos.indexOf(a);
+			}
+		}
+		if(b==-1)
+			throw new ImageNonExistanteError();
+		else
+			photos.remove(b);
+	}
+	public Album(){
+		photos = new Vector<A>();
+	}
+	
+	public String listephoto(){
+		String liste="";
+		for(A a : photos){
+			liste+=a.getchemin();
+		}
+		return liste;
+	}
+	
+	
+public static void main(String args[]){
+	Album a = new Album();
+	PhotoPNG pn = new PhotoPNG("chalhkh");
+	PhotoJpeg jp = new PhotoJpeg("miu_opjçjh");
+	PhotoPNG p = new PhotoPNG(50,50,100,100,"blub");
+	PhotoJpeg j = new PhotoJpeg(50,50,100,100,"blap");
+	
+	a.rajout(pn);
+	a.rajout(j);
+	System.out.println(" ");
+	System.out.println(a.listephoto());
+	a.rajout(jp);
+	a.rajout(p);
+	System.out.println(" ");
+	System.out.println(a.listephoto());
+	try{
+		a.enleve("blub");
+	}
+	catch(ImageNonExistanteError e) {System.out.println("erreur de suppression d'image");}
+	try{	
+		a.enleve("blap");
+	}
+	catch(ImageNonExistanteError e) {System.out.println("erreur de suppression d'image");}
+	System.out.println(" ");
+	System.out.println(a.listephoto());
+	
+	
+}
 
 }
